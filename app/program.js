8c16a6a3fc4adcca20b2aca9d717b3de79a7b7fc
Dagmara Bełczowska <dagmara.belczowska@wp.pl>
2019-02-15 22:37:10 +0100

    Add changes in TimeM.js

---
+++ app/program.js
@@ -0,0 +1,30 @@
+process.stdin.setEncoding('utf-8');
+process.stdin.on('readable', function() {
+    var input = process.stdin.read();
+    if (input !== null) {
+        var instruction = input.trim();
+        switch(instruction) {
+            case '/exit':
+                process.stdout.write('Quitting app!\n');
+                process.exit();
+                break;
+            case '/sayhello':
+                process.stdout.write('hello!\n');
+                break;
+            case '/getOSinfo':
+                getOSinfo();
+                break;
+            case '/getTime':
+                TimeM.print();
+                break;
+            default:
+                process.stderr.write('Wrong instruction!\n');
+        };
+    }
+});
+
+var OSinfo = require('../modules/OSinfo');
+var TimeM = require('../modules/TimeM');
+
+
+
--- modules/TimeM.js
+++ modules/TimeM.js
@@ -1,16 +1,14 @@
-var TimeM = require('TimeM');
+var os = require('os');
 
 function getTimeM() {
-	var minutes = Math.floor(time / 60);
-	var seconds = time - minutes * 60;
+	var time = os.uptime();
+	var minutes = Math.floor(time % 60);
+	var seconds = time % 60;
 	var hours = Math.floor(time / 3600);
 	time = time - hours * 3600;
 
-	var timeOperation = TimeM.timeOperation();
 	console.log('Time Operation:'+ hours + ':' + (minutes < 10 ? '0' + minutes : minutes) + ':' + (seconds < 10 ? '0' + seconds : seconds));
 }
 
 exports.print = getTimeM;
 
-var TimeM = require('./TimeM');
-TimeM.print();
